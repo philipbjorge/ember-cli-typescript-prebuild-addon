54d1a2e5e7e113ac151588608e318d2cd7cbc4e0
Philip Bjorge <philipbjorge@gmail.com>
2015-09-07 21:22:31 -0700

    See pwd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = {
-  name: 'ember-cli-rails-addon',
+  name: 'ember-cli-typescript-prebuild-addon',
   warnMissingDependencyChecker: function() {
     var dependencies = this.project.dependencies();
 
@@ -13,58 +13,25 @@ module.exports = {
 
   init: function() {
     this.warnMissingDependencyChecker();
-    this.ensureTmp();
   },
 
   buildError: function(error) {
-    fs.writeFileSync(this.errorFilePath(), error.stack)
+    console.warn(error);
   },
 
-  included: function(app) {
-    app.options.storeConfigInMeta = false;
-
-    if (process.env.DISABLE_FINGERPRINTING === 'true') {
-      app.options.fingerprint = app.options.fingerprint || {};
-      app.options.fingerprint.enabled = false;
-    }
-
-    if (process.env.EXCLUDE_EMBER_ASSETS) {
-      var excludeEmberAssets = process.env.EXCLUDE_EMBER_ASSETS;
-      var excludeRegex = new RegExp("(?:" + excludeEmberAssets.replace(",", "|") + ")\\.js$");
-      var excludeAssets = app.legacyFilesToAppend.filter(function(asset){ return excludeRegex.test(asset); });
-
-      excludeAssets.forEach(function(asset){
-        var index = app.legacyFilesToAppend.indexOf(asset);
-        app.legacyFilesToAppend.splice(index, 1);
-      });
-    }
-  },
   preBuild: function(result) {
-    var lockFile = this.lockFilePath();
-    var errorFile = this.errorFilePath();
-    if(!fs.existsSync(lockFile)) { fs.openSync(lockFile, 'w'); }
-    if(fs.existsSync(errorFile)) { fs.unlinkSync(errorFile); }
+    var sys = require('sys')
+    var exec = require('child_process').exec;
+    var child;
+
+    console.log(result);
+    // or more concisely
+    var sys = require('sys')
+    var exec = require('child_process').exec;
+    function puts(error, stdout, stderr) { sys.puts(stdout) }
+    exec("pwd", puts);
   },
   postBuild: function(result){
-    var lockFile = this.lockFilePath();
-    if(fs.existsSync(lockFile)) {
-      fs.unlinkSync(lockFile);
-    }
-  },
-
-  ensureTmp: function() {
-    var dir = this.tmpDir();
-    if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
-    }
-  },
-  tmpDir: function() {
-    return path.join(process.cwd(), 'tmp');
-  },
-  lockFilePath: function() {
-    return path.join(this.tmpDir(), 'build.lock');
-  },
-  errorFilePath: function() {
-    return path.join(this.tmpDir(), 'error.txt');
+    // TODO: Clean up
   }
 };
